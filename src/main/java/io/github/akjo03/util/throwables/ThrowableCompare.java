0adc6fc642ae3a2a4ec7d9a5cecd23e7b70d8519
AkjoHD <lukas.freckmann@gmx.ch>
2021-10-28 10:38:28 +0200

    Updated utility class for comparing Throwables

--- src/main/java/io/github/akjo03/util/throwables/ThrowableCompare.java
+++ src/main/java/io/github/akjo03/util/throwables/ThrowableCompare.java
@@ -10,8 +10,13 @@ public final class ThrowableCompare {
 	}
 
 	public static boolean compareThrowable(Throwable t1, Throwable t2) {
-		if (t1 == null) { return false; }
-		return t1.equals(t2) || (t1.getClass() == t2.getClass() && t1.getMessage().equals(t2.getMessage()) && compareStackTrace(t1.getStackTrace(), t2.getStackTrace()) && compareThrowable(t1.getCause(), t2.getCause()));
+		if (t1 == null && t2 == null) {
+			return true;
+		}
+		if (t1 == null) {
+			return false;
+		}
+		return t1.getClass() == t2.getClass() && t1.getMessage().equals(t2.getMessage()) && compareStackTrace(t1.getStackTrace(), t2.getStackTrace()) && compareThrowable(t1.getCause(), t2.getCause());
 	}
 
 	public static boolean compareStackTrace(StackTraceElement[] ste1, StackTraceElement[] ste2) {
